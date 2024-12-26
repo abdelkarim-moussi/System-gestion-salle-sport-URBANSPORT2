e6dd1890bd9d875f041b0a0864204961ef31cbca
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2024-12-26 10:29:44 +0100

    admin and member model files

---
+++ Model/modelAdmin.php
@@ -0,0 +1,28 @@
+<?php
+require_once("connexion.php");
+
+class modelAdmin{
+    private int $id;
+    private String $name;
+    private String $email;
+
+    public function __construct($id,$name,$email){
+        $this->$id = $id;
+        $this->$name = $name;
+        $this->$email = $email;
+
+    }
+
+
+
+    /** Les methodes magiques */
+
+    // public __get($test){
+    //     $this->teqt = $rze
+    // }
+}
+function selectSpecialities(){
+    $pdo = Connexion();
+    $stmt = $pdo->query("select * from public.specialite");
+    return $stmt;
+}
