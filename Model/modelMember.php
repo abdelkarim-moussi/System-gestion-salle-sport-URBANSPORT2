7b5773d9538f698d148d378926e4c3a1d82e19f0
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2024-12-26 16:16:46 +0100

    terminer la tache SDGSSV2-4-reservation

--- Model/modelMember.php
+++ Model/modelMember.php
@@ -14,5 +14,16 @@ class modelMemeber{
         $stmt->execute();
         return $stmt;
     }
+    
+    public function modifierInfos($id){
+        $stmt = $this->conn->prepare("UPDATE public.users
+        SET firstname=:firstname, lastname=:lastname, email=:email
+        WHERE  id = :id");
+        $stmt->bindParam(":firstname",$firstname,PDO::PARAM_STR);
+        $stmt->bindParam(":lastname",$lastname,PDO::PARAM_STR);
+        $stmt->bindParam(":email",$email,PDO::PARAM_STR);
+        $stmt->bindParam(":id",$id,PDO::PARAM_INT);
+        $stmt->execute();
+    }
 
 }
